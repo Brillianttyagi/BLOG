6452eebc52289847d23149c8eed0ba2be7f903f7
Brillianttyagi <deepanshubhai84@gmail.com>
2021-03-08 21:29:24 +0530

    added fully working navbar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 function expandnav() {
-    var change =  document.getElementsByClassName("links");
-    change.style.display = "block";
-  }
+    var change = document.getElementById("change");
+    if (change.style.display === "block"){
+        change.style.display = "none";
+    }
+    else{
+        change.style.display = "block";
+    }
+}
