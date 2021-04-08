0f8153f9d783a68241a2f9d7da4367e775fe05c1
Brillianttyagi <deepanshubhai84@gmail.com>
2021-04-08 20:24:23 +0530

    updated templates

--- django_app/brilliantprogrammer/bp/urls.py
+++ django_app/brilliantprogrammer/bp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from .  import views
 
 urlpatterns = [
-    path('',views.index,name='home')
+    path('',views.index,name='home'),
+    path('contactus',views.contact,name='contact'),
 ]
